37716789525866b9f4e9c1ee43d76c7a24ccafa0
Nauman <recluze@gmail.com>
2018-09-02 11:01:43 +0500

    Call changeset of cred on user insertion and deleteion

--- lib/foo/accounts/accounts.ex
+++ lib/foo/accounts/accounts.ex
@@ -6,7 +6,7 @@ defmodule Foo.Accounts do
   import Ecto.Query, warn: false
   alias Foo.Repo
 
-  alias Foo.Accounts.User
+  alias Foo.Accounts.{User, Credential}
 
   @doc """
   Returns the list of users.
@@ -58,6 +58,7 @@ defmodule Foo.Accounts do
   def create_user(attrs \\ %{}) do
     %User{}
     |> User.changeset(attrs)
+    |> Ecto.Changeset.cast_assoc(:credential, with: &Credential.changeset/2)
     |> Repo.insert()
   end
 
@@ -76,6 +77,7 @@ defmodule Foo.Accounts do
   def update_user(%User{} = user, attrs) do
     user
     |> User.changeset(attrs)
+    |> Ecto.Changeset.cast_assoc(:credential, with: &Credential.changeset/2)
     |> Repo.update()
   end
 
