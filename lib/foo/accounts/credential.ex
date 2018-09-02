fa73db466282616735a6faa79110f9df41dc749a
Nauman <recluze@gmail.com>
2018-09-02 10:43:13 +0500

    Add Credentials

---
+++ lib/foo/accounts/credential.ex
@@ -0,0 +1,20 @@
+defmodule Foo.Accounts.Credential do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+
+  schema "credentials" do
+    field :email, :string
+    field :user_id, :id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(credential, attrs) do
+    credential
+    |> cast(attrs, [:email])
+    |> validate_required([:email])
+    |> unique_constraint(:email)
+  end
+end
