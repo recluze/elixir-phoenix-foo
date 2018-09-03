cb74e18bc2d2eb7518d0de66f03f48f2ec0b18ab
Nauman <recluze@gmail.com>
2018-09-03 12:46:11 +0500

    When viewing pages, ensure correct author

--- lib/foo_web/controllers/page_controller.ex
+++ lib/foo_web/controllers/page_controller.ex
@@ -1,7 +1,31 @@
 defmodule FooWeb.PageController do
   use FooWeb, :controller
 
+  alias Foo.CMS
+
+  plug :require_existing_author
+  plug :authorize_page when action in [:edit, :update, :delete]
+
+
   def index(conn, _params) do
     render conn, "index.html"
+  end
+
+  defp require_existing_author(conn, _) do
+    author = CMS.ensure_author_exists(conn.assigns.current_user)
+    assign(conn, :current_author, author)
+  end
+
+  defp authorize_page(conn, _) do
+    page = CMS.get_page!(conn.params["id"])
+
+    if conn.assigns.current_author.id == page.author_id do
+      assign(conn, :page, page)
+    else
+      conn
+      |> put_flash(:error, "You can't modify that page")
+      |> redirect(to: cms_page_path(conn, :index))
+      |> halt()
+    end
   end
 end
