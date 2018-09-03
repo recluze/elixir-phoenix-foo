270d31a3eadc32ca72d30099fe231faf1b61f5c4
Nauman <recluze@gmail.com>
2018-09-03 12:03:34 +0500

    Create session controller and route

--- lib/foo/accounts/accounts.ex
+++ lib/foo/accounts/accounts.ex
@@ -110,6 +110,18 @@ defmodule Foo.Accounts do
     User.changeset(user, %{})
   end
 
+  def authenticate_by_email_password(email, _password) do
+    query =
+      from u in User,
+        inner_join: c in assoc(u, :credential),
+        where: c.email == ^email
+
+    case Repo.one(query) do
+      %User{} = user -> {:ok, user}
+      nil -> {:error, :unauthorized}
+    end
+  end
+
   alias Foo.Accounts.Credential
 
   @doc """
--- lib/foo_web/router.ex
+++ lib/foo_web/router.ex
@@ -18,6 +18,9 @@ defmodule FooWeb.Router do
 
     get "/", PageController, :index
     resources "/users", UserController
+
+    resources "/sessions", SessionController, only: [:new, :create, :delete],
+                                              singleton: true
   end
 
   # Other scopes may use custom stacks.
